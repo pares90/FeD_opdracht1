b886efd065a9f062db962d9b734d7bd04b37e145
pares90 <pares.scholberg@gmail.com>
2018-03-13 10:04:56 +0100

    Add files via upload

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,18 @@
-document.getElementsByClassName('favoriteunmarked').addEventlistener("click", myFunction);
 
-function myFunction() {
-    alert ("Hello World!");
+var button1 = document.querySelector('.button1');
+var button2 = document.querySelector('.button2');
+var sectionliked1 = document.querySelector('.section');
+
+
+function eersteFunctie(){
+  button1.classList.toggle('buttonclicked');
+  sectionliked.classList.toggle('sectionliked');
 }
+
+function tweedeFunctie(){
+  button2.classList.toggle('buttonclicked');
+  sectionliked.classList.toggle('sectionliked');
+}
+
+button1.addEventListener('click', eersteFunctie);
+button2.addEventListener('click', tweedeFunctie);
